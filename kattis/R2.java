fc0bf4ca44d45d0703f87f3c8751cbfe5548f718
LeffeMannen <andreas.ohlsson@elev.ga.ntig.se>
2019-09-27 09:27:51 +0200

    Kattis Uppgift

---
+++ kattis/R2.java
@@ -0,0 +1,17 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class R2 {
+
+	public static void main(String[] args) {
+		  
+		
+	Scanner input = new Scanner(System.in);
+	int r1 = input.nextInt();
+	int s = input.nextInt();
+  	System.out.println(s- (r1-s));
+  
+  
+}
+}
---
+++ kattis/Stuck_in_a_time_loop.java
@@ -0,0 +1,14 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class Stuck_in_a_time_loop {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+        int cases = input.nextInt();
+        for(int i = 1; i < cases+1; i++)
+            System.out.println(i + " Abracadabra");
+    }
+}
